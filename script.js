e609212d04e1b19d9774179d89a617fad7ba8107
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-23 20:51:10 -0300

    questao3

--- script.js
+++ script.js
@@ -1,25 +1,56 @@
-// //Questão 1 ---
+// // //Questão 1 ---
 
-// let tarefaConcluida = true;
+// // let tarefaConcluida = true;
 
-// if (tarefaConcluida === true) {
-//     console.log("A tarefa está concluída!");
+// // if (tarefaConcluida === true) {
+// //     console.log("A tarefa está concluída!");
+// // } else {
+// //     console.log("A tarefa não foi concluída.");
+// // }
+
+
+// // Questão 2 ---
+
+
+// let prioridade = 2; 
+
+// if (prioridade === 1) {
+//     console.log("Prioridade Baixa");
+// } else if (prioridade === 2) {
+//     console.log("Prioridade Média");
+// } else if (prioridade === 3) {
+//     console.log("Prioridade Alta");
 // } else {
-//     console.log("A tarefa não foi concluída.");
+//     console.log("Valor de prioridade inválido");
 // }
 
 
-// Questão 2 ---
+//Questão 3
 
+let diaSemana = new Date().getDay();
 
-let prioridade = 2; 
-
-if (prioridade === 1) {
-    console.log("Prioridade Baixa");
-} else if (prioridade === 2) {
-    console.log("Prioridade Média");
-} else if (prioridade === 3) {
-    console.log("Prioridade Alta");
-} else {
-    console.log("Valor de prioridade inválido");
+switch (diaSemana) {
+  case 0:
+    console.log("Domingo");
+    break;
+  case 1:
+    console.log("Segunda-feira");
+    break;
+  case 2:
+    console.log("Terça-feira");
+    break;
+  case 3:
+    console.log("Quarta-feira");
+    break;
+  case 4:
+    console.log("Quinta-feira");
+    break;
+  case 5:
+    console.log("Sexta-feira");
+    break;
+  case 6:
+    console.log("Sábado");
+    break;
+  default:
+    console.log("Dia inválido");
 }
