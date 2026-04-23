1a9eb5bda6c6fc98d5adbddf85cdd83c44558250
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-23 20:50:27 -0300

    questao2

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-//Questão 1 ---
+// //Questão 1 ---
 
-let tarefaConcluida = true;
+// let tarefaConcluida = true;
 
-if (tarefaConcluida === true) {
-    console.log("A tarefa está concluída!");
+// if (tarefaConcluida === true) {
+//     console.log("A tarefa está concluída!");
+// } else {
+//     console.log("A tarefa não foi concluída.");
+// }
+
+
+// Questão 2 ---
+
+
+let prioridade = 2; 
+
+if (prioridade === 1) {
+    console.log("Prioridade Baixa");
+} else if (prioridade === 2) {
+    console.log("Prioridade Média");
+} else if (prioridade === 3) {
+    console.log("Prioridade Alta");
 } else {
-    console.log("A tarefa não foi concluída.");
+    console.log("Valor de prioridade inválido");
 }
